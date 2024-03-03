9ddbaeaeff8f12ca8e1568e4912fb7be644b1e1b
Sofia Lynch <113479969+SofiaBLynch@users.noreply.github.com>
2024-03-03 00:47:19 -0500

    Remove excess files, add header

---
+++ decode-gators/src/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1 id='title'>Office Hours Calendar</h1>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default Header;
