65a0f67eefe01b79628eeb4f93722cf2bfcea358
Fiona Cahalan <fionamcahalan@gmail.com>
2024-03-24 18:17:23 -0400

    Appears to get Office Hours Calendar page to show up on hosted web page??? Not entirely sure what I changed to fix it

--- decode-gators/src/components/auth.js
+++ decode-gators/src/components/auth.js
@@ -1,8 +1,58 @@
+// FROM EXAMPLE IN REACT COURSES FOR BEGINNERS BY PEDROTECH: https://www.youtube.com/watch?v=2hR-uWjBAgw
+
+
+import { auth, googleProvider } from "../config/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
+
 export const Auth = () => {
-    return (
-        <div> 
-            <input placeholder ="Email..."/>
-            <input placeholder ="Password..."/>
-        </div>
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="Email..."
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        placeholder="Password..."
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={signIn}> Sign In</button>
+
+      <button onClick={signInWithGoogle}> Sign In With Google</button>
+
+      <button onClick={logout}> Logout </button>
+    </div>
+  );
 };
--- decode-gators/src/index.js
+++ decode-gators/src/index.js
@@ -8,8 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <AddHours />
+    <App />
   </React.StrictMode>
 );
 
