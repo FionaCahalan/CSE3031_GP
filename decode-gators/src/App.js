0896b5698608cf74db21bf6b122d688c295fa7c0
Fiona Cahalan <fionamcahalan@gmail.com>
2024-03-25 14:02:30 -0400

    fixing dependency problems

--- decode-gators/src/App.js
+++ decode-gators/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './Header';
 import AddHours from './AddHours';
