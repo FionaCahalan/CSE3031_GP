59745103d867fddb1285e70614c0747e5a30438f
Fiona Cahalan <fionamcahalan@gmail.com>
2024-03-25 12:32:07 -0400

    fixed errors causing deploy problems

--- decode-gators/src/App.js
+++ decode-gators/src/App.js
@@ -1,2 +1,19 @@
 import { useEffect, useState } from "react";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './Header';
+import AddHours from './AddHours';
 
+function App() {
+
+    return (
+      <Router>
+        <Header/>
+        
+        <Routes>
+          <Route path="/" element={<AddHours/>} />
+        </Routes>
+      </Router>
+    );
+  }
+  
+  export default App;
--- decode-gators/src/index.js
+++ decode-gators/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header from './Header';
-import AddHours from './AddHours';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
