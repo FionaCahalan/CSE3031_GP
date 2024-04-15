d29bbf3ce1581345c6df72c2795eb96ee2ffd915
Fiona Cahalan <fionamcahalan@gmail.com>
2024-04-14 21:30:18 -0400

    checks if user is admin

--- decode-gators/src/components/Homepage.jsx
+++ decode-gators/src/components/Homepage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
 import { db } from './../firebase';
-import { getDoc, doc, collection, getDocs} from 'firebase/firestore';
+import { getDoc, doc/*, collection, getDocs*/} from 'firebase/firestore';
 import './Homepage.css';
 
 const Homepage = () => {
@@ -18,7 +18,8 @@ function MyHomepage() {
     let navigate = useNavigate();
     const auth = getAuth();
     const [user, setUser] = useState(null);
-    const [isProfessor, setIsProfessor] = useState(false);
+    const [isProfessorTA, setIsProfessorTA] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(false);
 
     useEffect(() => {
       onAuthStateChanged(auth, currentUser => {
@@ -26,7 +27,8 @@ function MyHomepage() {
         if(currentUser){
           check(currentUser);
         } else {
-          setIsProfessor(false);
+          setIsProfessorTA(false);
+          setIsAdmin(false);
         }
       });
     }, []);
@@ -49,24 +51,49 @@ function MyHomepage() {
         const curr = doc(db, "users", email);
         getDoc(curr).then(async (snapshot)=> {
           const classes_is_prof = snapshot.data().Professor;
-          setIsProfessor(classes_is_prof.length > 0);
+          setIsProfessorTA(classes_is_prof.length > 0);
+          if(!isProfessorTA){
+            const classes_is_TA = snapshot.data().TA;
+            setIsProfessorTA(classes_is_TA.length > 0);
+          }
         });
-        console.log("Is Professor: ", isProfessor);
+        console.log("Is Professor: ", isProfessorTA);
       } else {
-        setIsProfessor(false);
+        setIsProfessorTA(false);
+      }
+
+      const docRef1 = doc(db, "admin", email);
+      const docSnap1 = await getDoc(docRef1);
+      if(docSnap1.exists()){
+        setIsAdmin(true);
+      } else {
+        setIsAdmin(false);
       }
     }
 
     return (
       <div>        
+        
         {!user ? (
           <button className="nav-button" type="button" onClick={() => handleClick('/login')}>Login</button>
         ) : (
           <>
           <button className = "nav-button" type="button" onClick={userSignOut}>Sign Out</button>
-          <button className="nav-button" type="button" onClick={() => handleClick('/calendar')}>Calendar</button>
 
-          {isProfessor && (<button className = "nav-button" type="button" onClick={() => handleClick('/addhours')}>Add Office Hours</button>)}
+          {isProfessorTA ? (
+            <>
+            <button className = "nav-button" type="button" onClick={() => handleClick('/addhours')}>Add Office Hours</button>
+            <button className="nav-button" type="button" onClick={() => handleClick('/calendar')}>Calendar</button>
+            </>
+          ) : (
+            <>
+            {isAdmin ? (
+              <button className="nav-button" type="button" onClick={() => handleClick('/admin')}>Admin</button>
+            ) :(
+              <button className="nav-button" type="button" onClick={() => handleClick('/calendar')}>Calendar</button>
+            )}
+            </>
+          )}
           </>
         )}
       </div>
